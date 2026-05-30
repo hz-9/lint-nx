b6eb1f57b47adcc12819b91d2595178e57029bf9
Chen Zhen <heavenzhen999@163.com>
2026-05-31 00:36:21 +0800

    fix: remove baseImportsRules spread from imports rules

---
+++ .changeset/curly-trees-refactor.md
@@ -0,0 +1,6 @@
+---
+'@hz-9/eslint-config-airbnb': patch
+'@hz-9/eslint-config-airbnb-ts': patch
+---
+
+fix: remove baseImportsRules spread from imports rules
--- packages/eslint-config-airbnb/rules/imports.js
+++ packages/eslint-config-airbnb/rules/imports.js
@@ -2,8 +2,6 @@ const { rules: baseImportsRules } = require('../airbnb-base-rules/imports')
 
 module.exports = {
   rules: {
-    ...baseImportsRules,
-
     // prefer default export when a module has a single export
     // https://github.com/import-js/eslint-plugin-import/blob/v2.26.0/docs/rules/prefer-default-export.md
     'import/prefer-default-export': 'off',
